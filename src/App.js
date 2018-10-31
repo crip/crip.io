a9c6e11e988b115ee114f70a8c2886e936c4c609
Johnie Hjelm <johnie.hjelm@mrggroup.com>
2018-10-31 22:29:59 +0100

    Added bugsnag

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import { Router, Link } from 'react-static';
 import { hot } from 'react-hot-loader';
-
+import config from '../config.json';
+import bugsnag from 'bugsnag-js';
+import createPlugin from 'bugsnag-react';
 import Routes from 'react-static-routes';
 
 import './app.css';
 
+const bugsnagClient = bugsnag(config.bugsnag.key);
+const ErrorBoundary = bugsnagClient.use(createPlugin(React));
+
 const App = () => (
-    <Router>
-        <div>
-            <nav>
-                <Link exact to="/">
-                    Home
-                </Link>
-                <Link to="/about">About</Link>
-                <Link to="/blog">Blog</Link>
-            </nav>
-            <div className="content">
-                <Routes />
+    <ErrorBoundary>
+        <Router>
+            <div>
+                <nav>
+                    <Link exact to="/">
+                        Home
+                    </Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/blog">Blog</Link>
+                </nav>
+                <div className="content">
+                    <Routes />
+                </div>
             </div>
-        </div>
-    </Router>
+        </Router>
+    </ErrorBoundary>
 );
 
 export default hot(module)(App);
