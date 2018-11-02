e6c8b117cfcf11bebb201324d97785285a6a494b
Johnie Hjelm <johnie.hjelm@mrggroup.com>
2018-11-02 16:35:59 +0100

    Added travis

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import { Router, Link } from 'react-static';
 import { hot } from 'react-hot-loader';
-
 import Routes from 'react-static-routes';
+import BugWrapper from './containers/BugWrapper';
 
 import './app.css';
 
 const App = () => (
-    <Router>
-        <div>
-            <nav>
-                <Link exact to="/">
-                    Home
-                </Link>
-                <Link to="/about">About</Link>
-                <Link to="/blog">Blog</Link>
-            </nav>
-            <div className="content">
-                <Routes />
+    <BugWrapper>
+        <Router>
+            <div>
+                <nav>
+                    <Link exact to="/">
+                        Home
+                    </Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/blog">Blog</Link>
+                </nav>
+                <div className="content">
+                    <Routes />
+                </div>
             </div>
-        </div>
-    </Router>
+        </Router>
+    </BugWrapper>
 );
 
 export default hot(module)(App);
