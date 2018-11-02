146f10e7364a422a10b43ea348b0080240395311
Johnie Hjelm <johniehjelm@me.com>
2018-11-02 21:38:24 +0100

    Added lint-staged, prettier, husky

--- src/containers/BugWrapper.js
+++ src/containers/BugWrapper.js
@@ -1,28 +1,25 @@
-import React, { Fragment } from 'react';
-import * as pkg from '../../package.json';
-import bugsnag from 'bugsnag-js';
-import createPlugin from 'bugsnag-react';
+import React, { Fragment } from 'react'
+import bugsnag from 'bugsnag-js'
+import createPlugin from 'bugsnag-react'
+import * as pkg from '../../package.json'
 
-try {
-  import config from '../../config.json';
-} catch (e) {}
+const config = require('../../config.json')
 
-const hasApiKey = Object.prototype.hasOwnProperty.apply(config.bugsnag, [
-  'key'
-]);
+const hasApiKey = Object.prototype.hasOwnProperty.apply(config.bugsnag, ['key'])
+
 const bugsnagClient = bugsnag({
   apiKey: hasApiKey ? config.bugsnag.key : process.env.BUGSNAG_KEY,
   appVersion: pkg.version
-});
+})
 
-const ErrorBoundary = bugsnagClient.use(createPlugin(React));
+const ErrorBoundary = bugsnagClient.use(createPlugin(React))
 
 const BugWrapper = ({ children }) => {
   if (hasApiKey) {
-    return <ErrorBoundary>{children}</ErrorBoundary>;
+    return <ErrorBoundary>{children}</ErrorBoundary>
   }
 
-  return <Fragment>{children}</Fragment>;
-};
+  return <Fragment>{children}</Fragment>
+}
 
-export default BugWrapper;
+export default BugWrapper
