50fd5e813ac9cce1babf71e47dfad90c84754bdd
Johnie Hjelm <johnie.hjelm@mrggroup.com>
2018-11-04 20:52:15 +0100

    Added logo component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import { Router, Link } from 'react-static';
+import { Router, Link, Head, withSiteData } from 'react-static';
 import { hot } from 'react-hot-loader';
+
 import Routes from 'react-static-routes';
-import BugWrapper from './containers/BugWrapper';
 
 import './app.css';
 
-const App = () => (
-    <BugWrapper>
-        <Router>
-            <div>
-                <nav>
-                    <Link exact to="/">
-                        Home
-                    </Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/blog">Blog</Link>
-                </nav>
-                <div className="content">
-                    <Routes />
-                </div>
-            </div>
-        </Router>
-    </BugWrapper>
-);
+const App = withSiteData(({ title, slogan }) => (
+  <Router>
+    <React.Fragment>
+      <Head
+        titleTemplate={`${title} - %s`}
+        defaultTitle={`${title} - ${slogan}`}
+      />
+      <nav>
+        <Link exact to="/">
+          Home
+        </Link>
+        <Link to="/about">About</Link>
+        <Link to="/blog">Blog</Link>
+      </nav>
+      <div className="content">
+        <Routes />
+      </div>
+    </React.Fragment>
+  </Router>
+));
 
 export default hot(module)(App);
--- static.config.js
+++ static.config.js
@@ -1,48 +1,49 @@
-import { reloadRoutes } from 'react-static/node'
-import jdown from 'jdown'
-import chokidar from 'chokidar'
+import { reloadRoutes } from 'react-static/node';
+import jdown from 'jdown';
+import chokidar from 'chokidar';
 
-chokidar.watch('content').on('all', () => reloadRoutes())
+chokidar.watch('content').on('all', () => reloadRoutes());
 
 export default {
   getSiteData: () => ({
     title: 'Crip in Tech',
+    slogan: 'Unlocking your potential'
   }),
   getRoutes: async () => {
-    const { posts, home, about } = await jdown('content')
+    const { posts, home, about } = await jdown('content');
     return [
       {
         path: '/',
         component: 'src/containers/Home',
         getData: () => ({
-          ...home,
-        }),
+          ...home
+        })
       },
       {
         path: '/about',
         component: 'src/containers/About',
         getData: () => ({
-          about,
-        }),
+          about
+        })
       },
       {
         path: '/blog',
         component: 'src/containers/Blog',
         getData: () => ({
-          posts,
+          posts
         }),
         children: posts.map(post => ({
           path: `/post/${post.slug}`,
           component: 'src/containers/Post',
           getData: () => ({
-            post,
-          }),
-        })),
+            post
+          })
+        }))
       },
       {
         is404: true,
-        component: 'src/containers/404',
-      },
-    ]
-  },
-}
+        component: 'src/containers/404'
+      }
+    ];
+  }
+};
