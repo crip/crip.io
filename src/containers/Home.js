e0a343471a70444d73e9a71ddf357c4ca5077765
Johnie Hjelm <johnie.hjelm@mrggroup.com>
2018-11-07 22:29:31 +0100

    Fixed travis

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { withRouteData } from 'react-static'
 import convert from 'htmr'
-import styled from 'styled-components'
-import { LogoFull } from '../components/Logo'
+import { LogoFull } from '../components/logo'
 //
 
 // const Header = styled.div`
@@ -15,13 +14,6 @@ import { LogoFull } from '../components/Logo'
 //   overflow: hidden;
 // `
 
-const Box = styled.div`
-  padding: 50px;
-  background-color: var(--note-color);
-  display: inline-block;
-  color: var(--main-color-dark);
-`
-
 export default withRouteData(({ jdown, reactStatic }) => (
   <div>
     <div className="header">
@@ -31,9 +23,5 @@ export default withRouteData(({ jdown, reactStatic }) => (
     <div className="header-after" />
     <section>{convert(reactStatic.contents)}</section>
     <section>{convert(jdown.contents)}</section>
-    <Box>
-      <h1>heeey</h1>
-      <p>helloworld ddkdk dmdml</p>
-    </Box>
   </div>
 ))
