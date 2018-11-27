2bf0eb0fea94b9de798fcf430c6df4b8798cfea0
Johnie Hjelm <johnie.hjelm@mrggroup.com>
2018-11-27 14:06:35 +0100

    Added Hamburger

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -9,7 +9,7 @@ const NavigationWrap = styled.div`
   top: 0;
   left: 0;
   width: 100%;
-  z-index: 10;
+  z-index: 20;
 `
 
 const Inner = styled(Flex)`
@@ -19,6 +19,77 @@ const Inner = styled(Flex)`
   @media (max-width: 32em) {
     .hide-on-mobile {
       display: none;
+    }
+  }
+`
+
+const Hamburger = styled.button`
+  position: relative;
+  display: block;
+  width: 25px;
+  height: 25px;
+  margin: 0;
+  padding: 0;
+  background: 0 0;
+  border: 0;
+  outline: 0;
+  transition: all 0s;
+  cursor: pointer;
+
+  span {
+    position: relative;
+    display: block;
+    width: 100%;
+    border-bottom: 2px white solid;
+    transition: width 0.3s, -webkit-transform 0.2s;
+    transition: transform 0.2s, width 0.3s;
+    transform-origin: center center;
+  }
+
+  span:last-child {
+    width: 14px;
+    margin: 6px 0 0 auto;
+  }
+
+  &.-active {
+    span {
+      width: 25px;
+      transform: translateY(4px) rotate(45deg);
+      transition-delay: 0.1s, 0s;
+    }
+
+    span:last-child {
+      transform: translateY(-4px) rotate(-45deg);
+    }
+  }
+
+  &::after {
+    content: '';
+    display: block;
+    position: absolute;
+    top: -15px;
+    left: -15px;
+    right: -15px;
+    bottom: -15px;
+    border-radius: 50%;
+    z-index: -1;
+    transition: all 0.3s;
+  }
+
+  &:hover,
+  &:focus {
+    span {
+      width: 14px;
+    }
+
+    span:last-child {
+      width: 100%;
+    }
+
+    &.-active {
+      span {
+        width: 25px;
+      }
     }
   }
 `
@@ -33,6 +104,9 @@ const Inner = styled(Flex)`
 // `
 
 class Navigation extends Component {
+  state = {
+    hm: '',
+  }
   render () {
     return (
       <NavigationWrap as="header" role="banner">
@@ -42,12 +116,17 @@ class Navigation extends Component {
               <LogoFull textClass="hide-on-mobile" />
             </Link>
           </Box>
-          {/* <Box>
-          <nav role="navigation">
-            <NavLink>Home</NavLink>
-            <NavLink to="/privacy">Privacy policy</NavLink>
-          </nav>
-        </Box> */}
+          <Box>
+            <Hamburger
+              onClick={() =>
+                this.setState(({ hm }) => ({ hm: hm === '' ? '-active' : '' })).bind(this)
+              }
+              className={this.state.hm}
+            >
+              <span />
+              <span />
+            </Hamburger>
+          </Box>
         </Inner>
       </NavigationWrap>
     )
