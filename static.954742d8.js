28c837ee9858bdff82a8a1dc7dc5488a35bf90e8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-27 21:33:39 +0000

    Deploy crip/crip.io to github.com/crip/crip.io.git:gh-pages

--- static.954742d8.js
+++ static.954742d8.js
@@ -67,7 +67,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "https://crip.io/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 15);
@@ -2702,4 +2702,4 @@ function toComment(sourceMap) {
 /***/ })
 /******/ ]);
 });
-//# sourceMappingURL=static.83de93ce.js.map
+//# sourceMappingURL=static.954742d8.js.map
